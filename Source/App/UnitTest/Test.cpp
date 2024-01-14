da11e06d71f1582ec7031953963bdfd087cab2de
WeslyChen <306325111@qq.com>
2024-01-14 13:56:41 +0800

    boost库

---
+++ Source/App/UnitTest/Test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by WeslyChen on 2024/1/14.
+//
+
+#include "Core/Core.h"
+#include "gtest/gtest.h"
+#include "boost/pool/pool.hpp"
+
+
+TEST(Test, Test)
+{
+
+}
--- Source/App/UnitTest/main.cpp
+++ Source/App/UnitTest/main.cpp
@@ -2,19 +2,10 @@
 // Created by chenwei on 2024/1/9.
 //
 
-#include "Core/Core.h"
 #include "gtest/gtest.h"
 
 int main(int argc, char* argv[])
 {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-}
-
-TEST(UnitTest, Test01)
-{
-    int32 test01 = 1;
-    int test02 = 1;
-
-    EXPECT_TRUE(test01 == test02);
 }
