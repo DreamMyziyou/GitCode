e95d30376dc3db206b481a63732d28329930628d
WeslyChen <306325111@qq.com>
2024-02-05 23:15:23 +0800

    fix example mesh

--- Example/UITest/Application.cpp
+++ Example/UITest/Application.cpp
@@ -15,7 +15,10 @@ Application::Application()
     auto entity = world->create();
 
     MeshComponent mesh;
-    mesh.vertices = {{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}}, {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}}, {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}};
+    mesh.vertices = {{{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+                     {{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
+                     {{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
+                     {{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}}};
     mesh.indices = {0, 1, 2, 2, 3, 0};
     world->emplace<MeshComponent>(entity, std::move(mesh));
 }
